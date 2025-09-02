fb1d2f364dbb370104a04aee278b92ec8a8dca13
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-02 11:01:12 -0300

    removed header and footer from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Container from "@/components/Container";
 import { FeaturedPost } from "@/components/FeaturedPost";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import PostsList from "@/components/PostsList";
 import SpinLoader from "@/components/SpinLoader";
 import { Suspense } from "react";
@@ -9,7 +7,6 @@ import { Suspense } from "react";
 export default function HomePage() {
   return (
     <Container>
-      <Header />
       <Suspense fallback={<SpinLoader />}>
         <FeaturedPost />
       </Suspense>

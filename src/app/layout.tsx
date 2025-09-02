157a1f19f7d77312e649590d6b47adcfae324973
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-02 10:37:07 -0300

    title template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: {
+    default: "The blog",
+    template: "%s | The blog",
+  },
+};
 
 export default function RootLayout({
   children,

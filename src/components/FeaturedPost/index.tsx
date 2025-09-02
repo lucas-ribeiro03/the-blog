0bd3d8e5520dee80f98f91ba223270aabb1eb698
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-02 11:07:18 -0300

    dinamically taking the first post as the featured post

--- src/components/FeaturedPost/index.tsx
+++ src/components/FeaturedPost/index.tsx
@@ -1,38 +1,28 @@
+import { postRepository } from "@/repositories/post";
 import { PostCoverImage } from "../PostCoverImage";
-import { PostHeading } from "../PostHeading";
+import { PostSummary } from "../PostSummary";
 
-export const FeaturedPost = () => {
+export const FeaturedPost = async () => {
+  const posts = await postRepository.findAllPublic();
+  const post = posts[0];
   return (
     <section className="grid grid-cols-1 gap-8 mb-5 sm:grid-cols-2 sm: items-center group">
       <PostCoverImage
         imageProps={{
-          alt: "Alt da imagem",
-          src: "/images/bryen_9.png",
+          alt: post.title,
+          src: post.coverImageUrl,
           width: 1200,
           height: 720,
           priority: true,
         }}
-        linkProps={{ href: "#" }}
+        linkProps={{ href: post.slug }}
       />
-      <div className="flex flex-col gap-4 sm:justify-center">
-        <time
-          dateTime="2025-08-29"
-          className="text-slate-600 block text-sm/tight"
-        >
-          29/08/2025 16:00
-        </time>
-
-        <PostHeading as="h1" url="#">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque,
-          optio.
-        </PostHeading>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          est odio minima nostrum id perferendis nisi veniam? Assumenda impedit
-          eligendi commodi quisquam consequatur? Quibusdam eum possimus quos,
-          quidem ex natus!
-        </p>
-      </div>
+      <PostSummary
+        createdAt={post.createdAt}
+        excerpt={post.excerpt}
+        title={post.title}
+        key={post.id}
+      />
     </section>
   );
 };
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -8,7 +8,7 @@ export default async function PostsList() {
   console.log(posts);
   return (
     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-      {posts.map((post) => (
+      {posts.slice(1).map((post) => (
         <div key={post.id} className="flex flex-col gap-4 group">
           <PostCoverImage
             imageProps={{

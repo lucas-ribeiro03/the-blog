d4b64742e04e3494c06aed99b05392b0c1ba4a8c
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-02 13:45:23 -0300

    href using post link

--- src/components/FeaturedPost/index.tsx
+++ src/components/FeaturedPost/index.tsx
@@ -5,7 +5,7 @@ import { PostSummary } from "../PostSummary";
 export const FeaturedPost = async () => {
   const posts = await postRepository.findAllPublic();
   const post = posts[0];
-  const postLink = `post/${post.slug}`;
+  const postLink = `/post/${post.slug}`;
   return (
     <section className="grid grid-cols-1 gap-8 mb-5 sm:grid-cols-2 sm: items-center group">
       <PostCoverImage
@@ -16,7 +16,7 @@ export const FeaturedPost = async () => {
           height: 720,
           priority: true,
         }}
-        linkProps={{ href: post.slug }}
+        linkProps={{ href: postLink }}
       />
       <PostSummary
         postLink={postLink}

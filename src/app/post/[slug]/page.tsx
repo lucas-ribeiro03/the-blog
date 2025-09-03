d1f91ff382745cbc612bb63e83ea19f58b177296
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-03 15:05:46 -0300

    not found redirect ionto the own function

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -6,9 +6,8 @@ type PostPageProps = {
 
 export default async function PostPage({ params }: PostPageProps) {
   const { slug } = await params;
-  console.log(slug);
 
   const post = await findPostBySlugCached(slug);
-  console.log(post);
+
   return <p>{post.title}</p>;
 }

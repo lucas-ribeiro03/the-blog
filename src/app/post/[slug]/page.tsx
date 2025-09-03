086c2419e6590ad1ac134ba98492857adc2f98cf
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-03 15:12:30 -0300

    metadata generator

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,20 @@
 import { findPostBySlugCached } from "@/lib/queries";
+import { Metadata } from "next";
 
 type PostPageProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: PostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const post = await findPostBySlugCached(slug);
+  return {
+    title: post.title,
+  };
+}
 
 export default async function PostPage({ params }: PostPageProps) {
   const { slug } = await params;

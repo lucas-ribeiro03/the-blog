dc1501b2216741b642850a2bbee3961b9bde1157
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-08 07:54:11 -0300

    table props

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,7 +1,30 @@
+import ReactMarkdown from "react-markdown";
+import rehypeSanitize from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
+
 type SafeMarkdownProps = {
   markdown: string;
 };
 
 export const SafeMarkdown = ({ markdown }: SafeMarkdownProps) => {
-  return <div>{markdown}</div>;
+  return (
+    <div className="w-full max-w-none overflow-hidden prose-a:text-blue-500 prose-a:hover:text-blue-700 prose-a:transition prose-img:mx-auto md:prose-lg">
+      <ReactMarkdown
+        rehypePlugins={[rehypeSanitize]}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return "";
+            return (
+              <div className="overflow-x-auto">
+                <table className="w-full min-w-[600px]" {...props} />
+              </div>
+            );
+          },
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  );
 };

8532d5e8646df1f7158b2dfcf9ebdf74e7946349
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-12 12:41:41 -0300

    db tests

---
+++ src/drizzle/tests.ts
@@ -0,0 +1,12 @@
+import { eq } from "drizzle-orm";
+import { drizzleDb } from ".";
+import { PostTable } from "./schemas";
+
+(async () => {
+  await drizzleDb
+    .update(PostTable)
+    .set({
+      title: "Rotina",
+    })
+    .where(eq(PostTable.slug, "rotina-matinal-de-pessoas-altamente-eficazes")); //Limpar a base de dados antes de inserir os dados
+})();

ea8e4849261df68236bbeea0c017b85d6afccb2d
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-17 09:09:11 -0300

    page title

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -1,6 +1,11 @@
 import { postRepository } from "@/repositories/post";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Post Admin",
+};
 
 export default async function AdminPostPage() {
   const posts = await postRepository.findAll();

a3f3a49b8bd5ffb1503c144ec7af1843c8dbada6
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-17 09:41:35 -0300

    moved admin and public routes into queries

---
+++ src/lib/queries/public.ts
@@ -0,0 +1,31 @@
+import { postRepository } from "@/repositories/post";
+import { unstable_cache } from "next/cache";
+import { notFound } from "next/navigation";
+import { cache } from "react";
+
+export const findAllPublicPostsPublic = cache(
+  unstable_cache(
+    async () => {
+      return await postRepository.findAllPublic();
+    },
+    ["posts"],
+    {
+      tags: ["posts"],
+      revalidate: 60,
+    }
+  )
+);
+
+export const findPostBySlugPublic = cache((slug: string) => {
+  return unstable_cache(
+    async (slug: string) => {
+      const post = await postRepository
+        .findBySlugPublic(slug)
+        .catch(() => undefined);
+      if (!post) notFound();
+      return post;
+    },
+    [`post-${slug}`],
+    { tags: [`post-${slug}`] }
+  )(slug);
+});

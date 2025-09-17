09ea713841b20a8948596dc27aed67991e485548
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-17 09:46:11 -0300

    function for delay

---
+++ src/utils/async-delay.ts
@@ -0,0 +1,9 @@
+export const asyncDelay = async (milisseconds: number = 0, verbose: false) => {
+  if (milisseconds) return;
+
+  if (verbose) {
+    console.log(`Delaying for ${milisseconds / 1000}s`);
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, milisseconds));
+};

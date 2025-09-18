c5e9345e02d73ee7facde288acc6aa0a92b7147f
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-18 09:53:56 -0300

    using dialog for confirmation

--- src/components/Admin/DeletePostButton/index.tsx
+++ src/components/Admin/DeletePostButton/index.tsx
@@ -1,23 +1,48 @@
 "use client";
 
+import { deletePostAction } from "@/actions/delete-post-action";
+import { Dialog } from "@/components/Dialog";
+import clsx from "clsx";
 import { Trash2Icon } from "lucide-react";
+import { useState, useTransition } from "react";
 
 type DeletePostButtonProps = {
   id: string;
 };
 
 export const DeletePostButton = ({ id }: DeletePostButtonProps) => {
-  const handleClick = () => {
-    console.log(id);
+  const [isPending, startTransition] = useTransition();
+  const [showDialog, setShowDialog] = useState(false);
+
+  const handleDeletePost = () => {
+    startTransition(async () => {
+      const result = await deletePostAction(id);
+      alert(result);
+      setShowDialog(false);
+    });
   };
   return (
-    <button
-      onClick={handleClick}
-      aria-label="Apagar post"
-      title="Apagar post"
-      className="text-red-500 cursor-pointer transition duration-200 [&_svg]:h-4 [&_svg]:w-4 hover:scale-150 hover:text-red-700"
-    >
-      <Trash2Icon />
-    </button>
+    <>
+      <button
+        onClick={() => setShowDialog(true)}
+        aria-label="Apagar post"
+        title="Apagar post"
+        className={clsx(
+          "text-red-500 cursor-pointer transition duration-200 [&_svg]:h-4 [&_svg]:w-4 hover:scale-150 hover:text-red-700 disabled:text-slate-300 disabled:cursor-not-allowed"
+        )}
+        disabled={isPending}
+      >
+        <Trash2Icon />
+      </button>
+      {showDialog && (
+        <Dialog
+          content="Deseja mesmo apagar o post?"
+          disabled={isPending}
+          onCancel={() => setShowDialog(false)}
+          onConfirm={handleDeletePost}
+          title="Apagar post?"
+        />
+      )}
+    </>
   );
 };

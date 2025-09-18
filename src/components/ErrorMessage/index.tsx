c5e5802040a6ac62877066621be67636c7535122
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-18 10:20:50 -0300

    page title optional

--- src/components/ErrorMessage/index.tsx
+++ src/components/ErrorMessage/index.tsx
@@ -1,5 +1,5 @@
 type ErrorMessageProps = {
-  pageTitle: string;
+  pageTitle?: string;
   contentTitle: string;
   content: string;
 };
@@ -11,7 +11,7 @@ export default function ErrorMessage({
 }: ErrorMessageProps) {
   return (
     <>
-      <title>{pageTitle}</title>
+      {pageTitle && <title>{pageTitle}</title>}
       <div className="min-h-[320px] bg-slate-900 text-slate-100 dark:bg-slate-100 dark:text-slate-900 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
         <div>
           <h1 className="text-7xl/tight mb-4 font-extrabold">{contentTitle}</h1>

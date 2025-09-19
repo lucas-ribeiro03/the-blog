9a8968b2cc36089082aa1e4967b491847f949636
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-19 01:27:23 -0300

    react toastify

---
+++ src/components/ToastifyContainer/indext.tsx
@@ -0,0 +1,19 @@
+import { Bounce, ToastContainer } from "react-toastify";
+
+export const ToastifyContainer = () => {
+  return (
+    <ToastContainer
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition={Bounce}
+    />
+  );
+};

279d750f17d068a790cac84e8b01263380cb05bb
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-22 08:32:01 -0300

    teste

--- src/app/admin/post/new_post/page.tsx
+++ src/app/admin/post/new_post/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/Button";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminNewPostPage() {
-  return <div className="py-6 text-6xl">AdminNewPostPage</div>;
+  return (
+    <div className="py-6 ">
+      <Button type="submit">Teste</Button>
+    </div>
+  );
 }

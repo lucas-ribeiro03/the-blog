5650169bdfba5b95771a4d9e1e8583c46e8065ac
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-22 08:59:22 -0300

    input text component

---
+++ src/components/InputText/index.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+import { useId } from "react";
+
+type InputTextProps = {
+  labelText?: string;
+} & React.ComponentProps<"input">;
+
+export const InputText = ({ labelText, ...props }: InputTextProps) => {
+  const id = useId();
+  return (
+    <div className="flex flex-col gap-2">
+      {labelText && (
+        <label className="text-sm" htmlFor={id}>
+          {labelText}
+        </label>
+      )}
+      <input
+        {...props}
+        className={clsx(
+          "bg-white",
+          "ring-1",
+          "ring-slate-400",
+          "rounded",
+          "p-2",
+          "text-base/tight",
+          "transition",
+          "duration-300",
+          "focus:ring-slate-blue-600",
+          "placeholder-slate-100",
+          "disabled:bg-slate-200",
+          "disabled:text-slate-400",
+          "disabled:placeholder-slate-400",
+          "read-only:bg-slate-200"
+        )}
+        id={id}
+      />
+    </div>
+  );
+};

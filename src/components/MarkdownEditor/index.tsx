9606e437b20dcfcd1028bd901abc1c2500cef0a0
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-22 15:02:41 -0300

    mark down editor component

---
+++ src/components/MarkdownEditor/index.tsx
@@ -0,0 +1,46 @@
+import React, { useId } from "react";
+import MDEditor from "@uiw/react-md-editor";
+
+type MarkdownEditorProps = {
+  labelText?: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  textAreaName: string;
+  disabled?: boolean;
+};
+
+export function MarkdownEditor({
+  labelText = "",
+  value,
+  setValue,
+  textAreaName,
+  disabled = false,
+}: MarkdownEditorProps) {
+  const id = useId();
+  return (
+    <div className="flex flex-col gap-2">
+      {labelText && (
+        <label className="text-sm" htmlFor={id}>
+          {labelText}
+        </label>
+      )}
+      <MDEditor
+        value={value}
+        onChange={(value) => {
+          if (!value) return;
+          setValue(value);
+        }}
+        height={400}
+        extraCommands={[]}
+        preview="edit"
+        hideToolbar={disabled}
+        textareaProps={{
+          id,
+          name: textAreaName,
+          disabled: disabled,
+        }}
+      />
+      <MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} />
+    </div>
+  );
+}

e2f537e1beecacb6fa3e0d2bfe5641ee85e31797
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-22 23:04:59 -0300

    markdown editor

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -1,13 +1,21 @@
 import { Button } from "@/components/Button";
 import { InputText } from "@/components/InputText";
+import { MarkdownEditor } from "@/components/MarkdownEditor";
+import { useState } from "react";
 
 export const ManagePostForm = () => {
+  const [contentValue, setContentValue] = useState("");
   return (
     <form className="flex flex-col gap-6">
       <InputText labelText="Nome" />
       <InputText labelText="Sobrenome" />
-      <InputText labelText="Nome" />
-      <InputText labelText="Sobrenome" />
+      <MarkdownEditor
+        labelText="Conteúdo"
+        disabled={false}
+        textAreaName="content"
+        value={contentValue}
+        setValue={setContentValue}
+      />
       <Button className="w-full mt-6" variant="default" size="md">
         Enviar
       </Button>

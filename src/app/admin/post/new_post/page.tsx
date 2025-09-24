993c893a9e6c510e54da7495118732b03296efae
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-24 14:35:09 -0300

    title and style

--- src/app/admin/post/new_post/page.tsx
+++ src/app/admin/post/new_post/page.tsx
@@ -1,10 +1,16 @@
 import { ManagePostForm } from "@/components/Admin/ManagePostForm";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
 
+export const metadata: Metadata = {
+  title: "Criar post",
+};
+
 export default async function AdminNewPostPage() {
   return (
-    <div className="py-6 ">
+    <div className="flex flex-col gap-6">
+      <h1 className="text-xl font-extrabold">Criar post</h1>
       <ManagePostForm />
     </div>
   );

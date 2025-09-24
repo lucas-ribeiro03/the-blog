c4bfa78377877b818284ed87063a6272a8249944
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-24 09:39:50 -0300

    image directory and url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,4 @@
 export const SIMULATE_WAIT_IN_MS = 0;
 export const IMAGE_UPLOAD_MAX_SIZE = 921600; //900 * 1024 = 921000kb
+export const IMAGE_UPLOAD_DIRECTORY = "uploads"; // /public/uploads
+export const IMAGE_SERVER_URL = "http://localhost:3000"; // http://localhost:3000/uploads/image.jpg

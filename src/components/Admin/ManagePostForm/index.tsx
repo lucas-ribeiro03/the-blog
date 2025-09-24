492d82d82a98c4cdaba6633100f808d70d54ac9c
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-24 13:07:59 -0300

    form to create post

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -4,14 +4,15 @@ import { InputText } from "@/components/InputText";
 import { MarkdownEditor } from "@/components/MarkdownEditor";
 import { useState } from "react";
 import { ImageUploader } from "../ImageUploader";
+import { InputCheckbox } from "@/components/InputCheckbox";
 
 export const ManagePostForm = () => {
   const [contentValue, setContentValue] = useState("");
   return (
     <form className="flex flex-col gap-6">
-      <ImageUploader />
-      <InputText labelText="Nome" />
-      <InputText labelText="Sobrenome" />
+      <InputText labelText="Autor" name="author" />
+      <InputText labelText="Título" name="title" />
+      <InputText labelText="Resumo" name="excerpt" />
       <MarkdownEditor
         labelText="Conteúdo"
         disabled={false}
@@ -19,6 +20,9 @@ export const ManagePostForm = () => {
         value={contentValue}
         setValue={setContentValue}
       />
+      <ImageUploader />
+      <InputText labelText="Url da imagem de capa" name="coverImageUrl" />
+      <InputCheckbox labelText="Público" name="published" />
       <Button className="w-full mt-6" variant="default" size="md">
         Enviar
       </Button>

e9cb7f0146d669a3362be9a382d48edd76d52d07
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-24 14:52:07 -0300

    returning a post

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,3 +1,7 @@
+import { ManagePostForm } from "@/components/Admin/ManagePostForm";
+import { MakePublic } from "@/dto/dto";
+import { findPostByIdAdmin } from "@/lib/queries/admin";
+
 export const dynamic = "force-dynamic";
 
 type AdminPostIdPageProps = {
@@ -10,5 +14,13 @@ export default async function AdminPostIdPage({
   params,
 }: AdminPostIdPageProps) {
   const { id } = await params;
-  return <div className="py-6 text-6xl">AdminPostIdPage {id}</div>;
+  const post = await findPostByIdAdmin(id);
+  if (!post) return;
+  const publicPost = MakePublic(post);
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className="text-xl font-extrabold">Editar post</h1>
+      <ManagePostForm publicPost={publicPost} />
+    </div>
+  );
 }

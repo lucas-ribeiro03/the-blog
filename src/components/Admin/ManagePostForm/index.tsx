2a751693206409b42addbffb38be149afdf03f2e
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-25 08:17:19 -0300

    returning public post if editing

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -5,24 +5,47 @@ import { MarkdownEditor } from "@/components/MarkdownEditor";
 import { useState } from "react";
 import { ImageUploader } from "../ImageUploader";
 import { InputCheckbox } from "@/components/InputCheckbox";
-
-export const ManagePostForm = () => {
+import { PublicPost } from "@/dto/dto";
+type ManagePostFormProps = {
+  publicPost?: PublicPost;
+};
+export const ManagePostForm = ({ publicPost }: ManagePostFormProps) => {
   const [contentValue, setContentValue] = useState("");
   return (
     <form className="flex flex-col gap-6">
-      <InputText labelText="Autor" name="author" />
-      <InputText labelText="Título" name="title" />
-      <InputText labelText="Resumo" name="excerpt" />
+      <InputText
+        labelText="Autor"
+        name="author"
+        defaultValue={publicPost?.author || ""}
+      />
+      <InputText
+        labelText="Título"
+        name="title"
+        defaultValue={publicPost?.title || ""}
+      />
+      <InputText
+        labelText="Resumo"
+        name="excerpt"
+        defaultValue={publicPost?.excerpt || ""}
+      />
       <MarkdownEditor
         labelText="Conteúdo"
         disabled={false}
         textAreaName="content"
-        value={contentValue}
+        value={!publicPost ? contentValue : publicPost.content}
         setValue={setContentValue}
       />
       <ImageUploader />
-      <InputText labelText="Url da imagem de capa" name="coverImageUrl" />
-      <InputCheckbox labelText="Público" name="published" />
+      <InputText
+        defaultValue={publicPost?.coverImageUrl || ""}
+        labelText="Url da imagem de capa"
+        name="coverImageUrl"
+      />
+      <InputCheckbox
+        labelText="Público"
+        name="published"
+        defaultChecked={publicPost?.published}
+      />
       <Button className="w-full mt-6" variant="default" size="md">
         Enviar
       </Button>

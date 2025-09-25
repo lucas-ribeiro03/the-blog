fcbcf91d6d086e98aa6c247e3e549d219f253cc8
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-25 09:16:34 -0300

    renaming function

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { ManagePostForm } from "@/components/Admin/ManagePostForm";
-import { MakePublic } from "@/dto/dto";
+import { MakePublicPostFromDb } from "@/dto/dto";
 import { findPostByIdAdmin } from "@/lib/queries/admin";
 
 export const dynamic = "force-dynamic";
@@ -16,7 +16,7 @@ export default async function AdminPostIdPage({
   const { id } = await params;
   const post = await findPostByIdAdmin(id);
   if (!post) return;
-  const publicPost = MakePublic(post);
+  const publicPost = MakePublicPostFromDb(post);
   return (
     <div className="flex flex-col gap-6">
       <h1 className="text-xl font-extrabold">Editar post</h1>

6a1689081cb7beb0d478b2674476861d1c390744
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-25 08:52:33 -0300

    making partial public post

--- src/dto/dto.ts
+++ src/dto/dto.ts
@@ -2,16 +2,22 @@ import { PostModel } from "@/models/post/post-model";
 
 export type PublicPost = Omit<PostModel, "updatedAt">;
 
-export const MakePublic = (post: PostModel): PublicPost => {
+export const MakePartialPublicPost = (
+  post?: Partial<PostModel>
+): PublicPost => {
   return {
-    author: post.author,
-    content: post.content,
-    coverImageUrl: post.coverImageUrl,
-    createdAt: post.createdAt,
-    excerpt: post.excerpt,
-    id: post.id,
-    published: post.published,
-    slug: post.slug,
-    title: post.title,
+    author: post?.author || "",
+    content: post?.content || "",
+    coverImageUrl: post?.coverImageUrl || "",
+    createdAt: post?.createdAt || "",
+    excerpt: post?.excerpt || "",
+    id: post?.id || "",
+    published: post?.published || false,
+    slug: post?.slug || "",
+    title: post?.title || "",
   };
+};
+
+export const MakePublicPostFromDb = (post: PostModel): PublicPost => {
+  return MakePartialPublicPost(post);
 };

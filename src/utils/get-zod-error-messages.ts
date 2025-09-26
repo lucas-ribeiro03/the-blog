52f70060f57ed72d781628ccb046aef2373465e3
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-26 08:57:36 -0300

    getting zod error messages

--- src/utils/get-zod-error-messages.ts
+++ src/utils/get-zod-error-messages.ts
@@ -1,11 +1,5 @@
 import { ZodError } from "zod";
 
 export function getZodErrorMessage(error: ZodError): string[] {
-  return Object.values(error)
-    .map((field) => {
-      if (Array.isArray(field)) return field;
-      return field?._errors || [];
-    })
-    .flat()
-    .filter(Boolean);
+  return error.issues.map((issue) => issue.message);
 }

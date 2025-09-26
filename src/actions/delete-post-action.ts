b0348b5915e1faaaa555e50071d4f1451578221a
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-26 10:45:02 -0300

    deleting post from db

--- src/actions/delete-post-action.ts
+++ src/actions/delete-post-action.ts
@@ -8,15 +8,22 @@ import { eq } from "drizzle-orm";
 import { revalidateTag } from "next/cache";
 
 export const deletePostAction = async (id: string) => {
-  await asyncDelay(2000);
   if (!id || typeof id !== "string") return { error: "Dados inválidos" };
-  const post = await postRepository.findById(id).catch(() => undefined);
-  if (!post)
-    return {
-      error: "Post não encontrado",
-    };
 
-  await drizzleDb.delete(PostTable).where(eq(PostTable.id, id));
+  let post;
+  try {
+    post = await postRepository.delete(id);
+  } catch (e) {
+    if (e instanceof Error) {
+      return {
+        errors: e.message,
+      };
+    }
+
+    return {
+      errors: "Erro desconhecido",
+    };
+  }
   revalidateTag("posts");
   revalidateTag(`post-${post.slug}`);
 };

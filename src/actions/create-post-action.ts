913f966ebb7e17260535943ad697467159363ce2
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-26 09:39:28 -0300

    inserting on db, redirecting, and revalidating cache

--- src/actions/create-post-action.ts
+++ src/actions/create-post-action.ts
@@ -1,9 +1,13 @@
 "use server";
+import { drizzleDb } from "@/drizzle";
+import { PostTable } from "@/drizzle/schemas";
 import { MakePartialPublicPost, PublicPost } from "@/dto/dto";
 import { postCreateSchema } from "@/lib/post/validation";
 import { PostModel } from "@/models/post/post-model";
 import { getZodErrorMessage } from "@/utils/get-zod-error-messages";
 import { makeSlugFromText } from "@/utils/make-slug-from-text";
+import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
 import { v4 as uuidV4 } from "uuid";
 
 type createPostActionState = {
@@ -33,6 +37,10 @@ export const createPostAction = async (
     id: uuidV4(),
     slug: makeSlugFromText(validPostData.title),
   };
+
+  await drizzleDb.insert(PostTable).values(newPost);
+  revalidateTag("posts");
+  redirect(`/admin/post/${newPost.id}`);
 
   return {
     formState: newPost,

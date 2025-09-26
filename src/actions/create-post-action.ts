d638d02b8fa68710a1520d313bb5be0ca3fea26b
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-26 09:28:33 -0300

    creating a new post

--- src/actions/create-post-action.ts
+++ src/actions/create-post-action.ts
@@ -1,7 +1,10 @@
 "use server";
 import { MakePartialPublicPost, PublicPost } from "@/dto/dto";
 import { postCreateSchema } from "@/lib/post/validation";
+import { PostModel } from "@/models/post/post-model";
 import { getZodErrorMessage } from "@/utils/get-zod-error-messages";
+import { makeSlugFromText } from "@/utils/make-slug-from-text";
+import { v4 as uuidV4 } from "uuid";
 
 type createPostActionState = {
   formState: PublicPost;
@@ -22,8 +25,17 @@ export const createPostAction = async (
     };
   }
 
+  const validPostData = zodParsedObj.data;
+  const newPost: PostModel = {
+    ...validPostData,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    id: uuidV4(),
+    slug: makeSlugFromText(validPostData.title),
+  };
+
   return {
-    formState: { ...prevState.formState },
+    formState: newPost,
     errors: [],
   };
 };

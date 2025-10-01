6b7e46a6eb2897eeeb1ea356ae1b43a1f96ebb28
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-01 00:07:22 -0300

    action

--- src/components/Admin/LoginForm/index.tsx
+++ src/components/Admin/LoginForm/index.tsx
@@ -1,18 +1,36 @@
+"use client";
+
+import { loginAction } from "@/actions/login/login-action";
 import { Button } from "@/components/Button";
 import { InputText } from "@/components/InputText";
 import { LogInIcon } from "lucide-react";
+import { useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 export const LoginForm = () => {
+  const initialState = {
+    username: "",
+    error: "",
+  };
+  const [state, action, isPending] = useActionState(loginAction, initialState);
+
+  useEffect(() => {
+    if (state.error) {
+      toast.dismiss();
+      toast.error(state.error);
+    }
+  }, [state]);
+
   return (
     <div className="flex items-center justify-center text-center max-w-sm mt-16 mb-32 mx-auto">
-      <form action="" className="flex-1 flex flex-col gap-6">
+      <form action={action} className="flex-1 flex flex-col gap-6">
         <InputText
           type="text"
           name="username"
           labelText="Usuário"
           placeholder="Seu usuário"
-          disabled={false}
-          defaultValue={""}
+          disabled={isPending}
+          defaultValue={state.username}
         />
 
         <InputText
@@ -20,11 +38,11 @@ export const LoginForm = () => {
           name="password"
           labelText="Senha"
           placeholder="Sua senha"
-          disabled={false}
+          disabled={isPending}
           defaultValue={""}
         />
 
-        <Button disabled={false} type="submit" className="mt-4">
+        <Button disabled={isPending} type="submit" className="mt-4">
           <LogInIcon />
           Entrar
         </Button>

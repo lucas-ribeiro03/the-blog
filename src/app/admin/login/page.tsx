bfab3c19e1a9b079921701fda415a24328ce6063
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-09-30 23:58:00 -0300

    rendering login form

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,5 +1,11 @@
+import { LoginForm } from "@/components/Admin/LoginForm";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminLoginPage() {
-  return <div className="py-6 text-6xl">AdminLoginPage</div>;
+  return (
+    <div className="py-6 text-6xl">
+      <LoginForm />
+    </div>
+  );
 }

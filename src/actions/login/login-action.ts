c286ae6a98c04f83913c9523a57a94bed49d99d3
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-01 00:34:13 -0300

    login validations

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,7 +1,51 @@
 "use server";
 
+import { verifyPassword } from "@/lib/login/manage-login";
 import { asyncDelay } from "@/utils/async-delay";
 
-export const loginAction = async () => {
+type LoginActionState = {
+  username: string;
+  error: string;
+};
+
+export const loginAction = async (
+  state: LoginActionState,
+  formData: FormData
+) => {
   await asyncDelay(3000);
+
+  if (!(formData instanceof FormData)) {
+    return {
+      username: "",
+      error: "Dados inválidos",
+    };
+  }
+
+  const username = formData.get("username")?.toString().trim() || undefined;
+  const password = formData.get("password")?.toString().trim() || undefined;
+
+  if (!username || !password) {
+    return {
+      username: "",
+      error: "Digite o usuário e a senha",
+    };
+  }
+
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = await verifyPassword(
+    password,
+    process.env.LOGIN_PASS || ""
+  );
+
+  if (!isUsernameValid || !isPasswordValid) {
+    return {
+      username: "",
+      error: "Usuário ou senha inválidos",
+    };
+  }
+
+  return {
+    username: "",
+    error: "SUCESSO",
+  };
 };

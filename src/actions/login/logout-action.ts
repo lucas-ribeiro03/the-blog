9bbba5db1bc457e665b79f129c2e8e626c59bf61
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-02 10:57:44 -0300

    logout

--- src/actions/login/logout-action.ts
+++ src/actions/login/logout-action.ts
@@ -1,7 +1,11 @@
 "use server";
 
+import { deleteLoginSession } from "@/lib/login/manage-login";
 import { asyncDelay } from "@/utils/async-delay";
+import { redirect } from "next/navigation";
 
 export const logoutAction = async () => {
   await asyncDelay(3000);
+  await deleteLoginSession();
+  redirect("/");
 };

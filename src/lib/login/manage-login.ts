8a7047f043af29419868e70d8f94d67984b3decc
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-02 09:13:09 -0300

    create and delete login session

--- src/lib/login/manage-login.ts
+++ src/lib/login/manage-login.ts
@@ -1,4 +1,12 @@
 import bcrypt from "bcryptjs";
+import { cookies } from "next/headers";
+
+const jwtSecretKey = process.env.JWT_SECRET_KEY;
+const jwtEncodeKey = new TextEncoder().encode(jwtSecretKey);
+
+const loginExpSeconds = Number(process.env.LOGIN_EXPIRATION_SECONDS) || 86400;
+const loginExpString = process.env.LOGIN_EXPIRATION_STRING || "1d";
+const loginCookieName = process.env.LOGIN_COOKIE_NAME || "loginSession";
 
 export const hashPassword = async (password: string) => {
   const hash = await bcrypt.hash(password, 10);
@@ -13,4 +21,21 @@ export const verifyPassword = async (
 ) => {
   const hashedPassword = Buffer.from(hashedBase64, "base64").toString("utf-8");
   return bcrypt.compare(password, hashedPassword);
+};
+
+export const createLoginSession = async (username: string) => {
+  const expiresAt = new Date(Date.now() + loginExpSeconds * 1000);
+  const loginSession = username + "JWT";
+  const cookieStore = await cookies();
+  cookieStore.set(loginCookieName, loginSession, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    expires: expiresAt,
+  });
+};
+
+export const deleteLoginSession = async () => {
+  const cookieStore = await cookies();
+  cookieStore.delete(loginCookieName);
 };

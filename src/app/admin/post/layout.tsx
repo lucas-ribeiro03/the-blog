8930bf74d1942929703520c3d6ee3062589bf753
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-02 10:52:21 -0300

    verifying if user is authenticated

--- src/app/admin/post/layout.tsx
+++ src/app/admin/post/layout.tsx
@@ -1,12 +1,14 @@
 import { MenuAdmin } from "@/components/Admin/MenuAdmin";
+import { verifyLoginSession } from "@/lib/login/manage-login";
 
 type AdminPostsLayoutProps = {
   children: React.ReactNode;
 };
 
-export default function AdminPostsLayout({
+export default async function AdminPostsLayout({
   children,
 }: Readonly<AdminPostsLayoutProps>) {
+  await verifyLoginSession();
   return (
     <>
       <MenuAdmin />
